3d738dbc08f6d023f702012e7f55339d8e390703
katagaki <68231035+katagaki@users.noreply.github.com>
2023-09-01 13:11:56 +0800

    Initial Commit

---
+++ DiceyDice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DiceyDice
+//
+//  Created by シン・ジャスティン on 1/9/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DiceyDice/DiceyDiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceyDiceApp.swift
+//  DiceyDice
+//
+//  Created by シン・ジャスティン on 1/9/23.
+//
+
+import SwiftUI
+
+@main
+struct DiceyDiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
