bcce0f0a27c4298e3499beb5fe1f6daf1878ede1
katagaki <68231035+katagaki@users.noreply.github.com>
2023-09-01 23:52:35 +0800

    Create box for dice to be in

--- DiceyDice/ContentView.swift
+++ DiceyDice/ContentView.swift
@@ -13,33 +13,16 @@ struct ContentView: View {
 
     let scene = SCNScene()
 
-    @State var centerOfMassX: String = "1.68"
-    @State var centerOfMassY: String = "1.80"
-    @State var centerOfMassZ: String = "-0.78"
-
     var body: some View {
         VStack(alignment: .center, spacing: 8.0) {
             SceneView(scene: {
-                                 scene.background.contents = UIColor.systemGroupedBackground
+                                 scene.background.contents = UIColor.systemBackground
                                  return scene
                              }(),
                       pointOfView: camera(),
                       options: [.allowsCameraControl],
                       preferredFramesPerSecond: 60)
-            HStack {
-                TextField(text: $centerOfMassX) {
-                    Text("X")
-                }
-                TextField(text: $centerOfMassY) {
-                    Text("Y")
-                }
-                TextField(text: $centerOfMassZ) {
-                    Text("Z")
-                }
-            }
-            .textFieldStyle(.roundedBorder)
-            .padding()
-            Text("Default: 1.68, 1.80, -0.78")
+            Divider()
             HStack(alignment: .center, spacing: 8.0) {
                 Button {
                     resetScene()
@@ -65,6 +48,7 @@ struct ContentView: View {
                 .clipShape(RoundedRectangle(cornerRadius: 99))
                 .buttonStyle(.borderedProminent)
             }
+            .padding()
         }
         .onAppear {
             resetScene()
@@ -73,26 +57,24 @@ struct ContentView: View {
     }
 
     func addDice() {
-        scene.rootNode.addChildNode(dice(position: SCNVector3(-8, 4, 0),
+        // 1 facing up
+        scene.rootNode.addChildNode(dice(position: SCNVector3(-13, 2, 0),
                                          rotation: SCNVector3(0, 0, 0)))
-        
-        scene.rootNode.addChildNode(dice(position: SCNVector3(-4, 4, 0),
-                                         rotation: SCNVector3(3.14 / 2, 0, 0)))
-        
-        scene.rootNode.addChildNode(dice(position: SCNVector3(0, 4, 0),
-                                         rotation: SCNVector3(3.14, 0, 0)))
-        
-        scene.rootNode.addChildNode(dice(position: SCNVector3(4, 4, 0),
-                                         rotation: SCNVector3(0, 0, 3.14 / 2)))
-        
-        scene.rootNode.addChildNode(dice(position: SCNVector3(8, 4, 0),
-                                         rotation: SCNVector3(0, 0, 3.14)))
-        
-        scene.rootNode.addChildNode(dice(position: SCNVector3(-8, 4, 5),
+        // 2 facing up
+        scene.rootNode.addChildNode(dice(position: SCNVector3(-6, 4.26, 0),
                                          rotation: SCNVector3(3.14 / 2, 3.14 / 2, 3.14)))
-        
-        scene.rootNode.addChildNode(dice(position: SCNVector3(-4, 4, 5),
+        // 3 facing up
+        scene.rootNode.addChildNode(dice(position: SCNVector3(-1, 2, 0),
+                                         rotation: SCNVector3(0, 0, 3.14 / 2)))
+        // 4 facing up
+        scene.rootNode.addChildNode(dice(position: SCNVector3(4, 4.26, -2.5),
                                          rotation: SCNVector3(0, 3.14, 3.14 / 2)))
+        // 5 facing up
+        scene.rootNode.addChildNode(dice(position: SCNVector3(6, 2, -2.5),
+                                         rotation: SCNVector3(3.14 / 2, 0, 0)))
+        // 6 facing up
+        scene.rootNode.addChildNode(dice(position: SCNVector3(11, 4.26, -2.5),
+                                         rotation: SCNVector3(3.14, 0, 0)))
     }
 
     func resetScene() {
@@ -100,6 +82,9 @@ struct ContentView: View {
             node.removeFromParentNode()
         }
         scene.rootNode.addChildNode(floor())
+        for wall in walls() {
+            scene.rootNode.addChildNode(wall)
+        }
         scene.rootNode.addChildNode(light())
     }
 
@@ -108,22 +93,45 @@ struct ContentView: View {
         let diceNode = diceScene!.rootNode.childNodes[0]
         let physicsBody = SCNPhysicsBody(type: .dynamic, shape: nil)
         physicsBody.mass = 0.045
-        physicsBody.centerOfMassOffset = SCNVector3(Float(centerOfMassX)!,
-                                                    Float(centerOfMassY)!,
-                                                    Float(centerOfMassZ)!)
+        physicsBody.centerOfMassOffset = SCNVector3(1.70, 1.80, -0.96)
         diceNode.physicsBody = physicsBody
         diceNode.eulerAngles = rotation
         diceNode.position = position
         return diceNode
+    }
+    
+    func walls() -> [SCNNode] {
+        let leftWall = SCNBox(width: 1.0, height: 10.0, length: 40.0, chamferRadius: 0.0)
+        let leftWallNode = SCNNode(geometry: leftWall)
+        let rightWall = SCNBox(width: 1.0, height: 10.0, length: 40.0, chamferRadius: 0.0)
+        let rightWallNode = SCNNode(geometry: rightWall)
+        let topWall = SCNBox(width: 42.0, height: 10.0, length: 1.0, chamferRadius: 0.0)
+        let topWallNode = SCNNode(geometry: topWall)
+        let bottomWall = SCNBox(width: 42.0, height: 10.0, length: 1.0, chamferRadius: 0.0)
+        let bottomWallNode = SCNNode(geometry: bottomWall)
+        leftWallNode.position = SCNVector3(x: -20.5, y: 5, z: 0)
+        leftWallNode.geometry?.firstMaterial?.diffuse.contents = UIColor.lightGray
+        leftWallNode.physicsBody = SCNPhysicsBody(type: .static, shape: nil)
+        rightWallNode.position = SCNVector3(x: 20.5, y: 5, z: 0)
+        rightWallNode.geometry?.firstMaterial?.diffuse.contents = UIColor.lightGray
+        rightWallNode.physicsBody = SCNPhysicsBody(type: .static, shape: nil)
+        topWallNode.position = SCNVector3(x: 0, y: 5, z: 20.5)
+        topWallNode.geometry?.firstMaterial?.diffuse.contents = UIColor.lightGray
+        topWallNode.physicsBody = SCNPhysicsBody(type: .static, shape: nil)
+        bottomWallNode.position = SCNVector3(x: 0, y: 5, z: -20.5)
+        bottomWallNode.geometry?.firstMaterial?.diffuse.contents = UIColor.lightGray
+        bottomWallNode.physicsBody = SCNPhysicsBody(type: .static, shape: nil)
+        return [leftWallNode, rightWallNode, topWallNode, bottomWallNode]
     }
 
     func floor() -> SCNNode {
         let floor = SCNFloor()
         let floorNode = SCNNode(geometry: floor)
         floor.reflectivity = 0.5
-        floor.width = 10
-        floor.length = 10
-        floorNode.geometry?.firstMaterial?.diffuse.contents = UIColor.label
+        floor.width = 20.0
+        floor.length = 20.0
+        floor.reflectionFalloffEnd = 1.0
+        floorNode.geometry?.firstMaterial?.diffuse.contents = UIColor.gray
         floorNode.physicsBody = SCNPhysicsBody(type: .static, shape: nil)
         return floorNode
     }
