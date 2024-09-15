9add15a7e746a5ca45c387e9989c87a16f6cc87f
katagaki <68231035+katagaki@users.noreply.github.com>
2024-09-15 23:43:08 +0900

    Update for iOS 18 Implement Komponents

--- DiceyDice/MoreView.swift
+++ DiceyDice/MoreView.swift
@@ -5,54 +5,16 @@
 //  Created by シン・ジャスティン on 4/9/23.
 //
 
+import Komponents
 import SwiftUI
 
 struct MoreView: View {
 
     var body: some View {
         NavigationStack {
-            List {
-                Section {
-                    Link(destination: URL(string: "https://x.com/katagaki_")!) {
-                        HStack {
-                            ListRow(image: "ListIcon.Twitter",
-                                    title: "More.Help.Twitter",
-                                    subtitle: "More.Help.Twitter.Subtitle",
-                                    includeSpacer: true)
-                            Image(systemName: "safari")
-                                .opacity(0.5)
-                        }
-                        .foregroundColor(.primary)
-                    }
-                    Link(destination: URL(string: "mailto:ktgk.public@icloud.com")!) {
-                        HStack {
-                            ListRow(image: "ListIcon.Email",
-                                    title: "More.Help.Email",
-                                    subtitle: "More.Help.Email.Subtitle",
-                                    includeSpacer: true)
-                            Image(systemName: "arrow.up.forward.app")
-                                .opacity(0.5)
-                        }
-                        .foregroundColor(.primary)
-                    }
-                    Link(destination: URL(string: "https://github.com/katagaki/DiceyDice")!) {
-                        HStack {
-                            ListRow(image: "ListIcon.GitHub",
-                                    title: "More.Help.GitHub",
-                                    subtitle: "More.Help.GitHub.Subtitle",
-                                    includeSpacer: true)
-                            Image(systemName: "safari")
-                                .opacity(0.5)
-                        }
-                        .foregroundColor(.primary)
-                    }
-                } header: {
-                    ListSectionHeader(text: "More.Help")
-                        .font(.body)
-                }
-            }
-            .listStyle(.insetGrouped)
+            MoreList(repoName: "katagaki/DiceyDice") { }
             .navigationTitle("View.More")
+            .navigationBarTitleDisplayMode(.inline)
         }
     }
 }
